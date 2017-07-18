083f7de39b332021c337d9f4398fc24970d15167
pltc325 <pltc325@msn.com>
2017-07-19 07:47:41 +0800

    del pic

--- line_search/line_search.py
+++ line_search/line_search.py
@@ -12,19 +12,17 @@ def g_unit(xx1, xx2):
     return g_x1 / g_norm, g_x2 / g_norm, g_norm
 
 
-def line_search(x1,x2,t):
+def line_search(x1,x2,t,alpha,beta):
     f_current = f(x1,x2)
     g_unit_x1, g_unit_x2, g_norm = g_unit(x1,x2)
     f_new = f(x1 - t*g_unit_x1, x2 - t*g_unit_x2)
-    alpha = 0.5
-    beta = 0.8
     while f_new > f_current + alpha * t * np.sqrt(g_norm):
         t *= beta
         f_new = f(x1 - t*g_unit_x1, x2 - t*g_unit_x2)
     return t
 
 
-def plot(min_x1,max_x1,min_x2,max_x2,x1_start,x2_start,t,step_num,plot_no,is_line_search):
+def plot(min_x1,max_x1,min_x2,max_x2,x1_start,x2_start,t,step_num,plot_no,is_line_search,alpha=0.5,beta=0.8):
     plt.subplot(3,1,plot_no)
     x1 = np.arange(min_x1, max_x1, 1)
     x2 = np.arange(min_x2, max_x2, 1)
@@ -43,7 +41,7 @@ def plot(min_x1,max_x1,min_x2,max_x2,x1_start,x2_start,t,step_num,plot_no,is_lin
         x1s[i] = x1
         x2s[i] = x2
         if is_line_search:
-            t_c = line_search(x1,x2,t)
+            t_c = line_search(x1,x2,t,alpha,beta)
         delta_x1 = -t_c * g_unit(x1, x2)[0]
         delta_x2 = -t_c * g_unit(x1, x2)[1]
         x1 += delta_x1
@@ -55,6 +53,6 @@ if __name__ == "__main__":
     plt.figure(figsize=(5,16))
     plot(min_x1=-75,max_x1=75,min_x2=-75,max_x2=75,x1_start=-1,x2_start=20,t=100,step_num=1000,plot_no=1,is_line_search=False)
     plot(min_x1=-20,max_x1=20,min_x2=-20,max_x2=20,x1_start=-1,x2_start=20,t=0.01,step_num=1000,plot_no=2,is_line_search=False)
-    plot(min_x1=-20,max_x1=20,min_x2=-20,max_x2=20,x1_start=-1,x2_start=20,t=1,step_num=22,plot_no=3,is_line_search=True)
+    plot(min_x1=-20,max_x1=20,min_x2=-20,max_x2=20,x1_start=-1,x2_start=20,t=1,step_num=22,plot_no=3,is_line_search=True,alpha=0.5,beta=0.8)
     plt.savefig('books_read.png')
     plt.show()
